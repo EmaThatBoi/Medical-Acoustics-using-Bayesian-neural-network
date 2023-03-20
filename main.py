a1ee8ebbe6c585381c0dc76e654239f31bc59573
Emanuele Peratello <116254592+EmaThatBoi@users.noreply.github.com>
2023-03-20 14:59:42 +0100

    Update main.py
    modello che usa Sequential di tf.keras.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 import tensorflow as tf
 import functions
 import os
-from tensorflow import keras
 from tensorflow_probability.python.layers import Convolution2DFlipout, DenseFlipout
 from tensorflow.python.keras.layers import MaxPooling2D, Flatten, Dropout
 
@@ -28,20 +27,34 @@ x_data, y_data = functions.prepare_data(data, dir_label)
 
 x_data = np.array(x_data, dtype=np.float32)
 y_data = np.array(y_data, dtype=np.float32)
+
+# Get the number of samples
+num_samples = x_data.shape[0]
+
+# Shuffle dei dati
+indices = np.random.permutation(num_samples)
+x_data = x_data[indices]
+y_data = y_data[indices]
+
+# numero di campioni per ogni set
+train_ratio = 0.7
+val_ratio = 0.15
+test_ratio = 0.15
+num_train_samples = int(train_ratio * num_samples)
+num_val_samples = int(val_ratio * num_samples)
+num_test_samples = int(test_ratio * num_samples)
+
 x_data = tf.expand_dims(x_data,axis=3,name='channels')
-training_dataset = tf.data.Dataset.from_tensor_slices((x_data, y_data))
 
-# splitta il dataset in train,validation, e test set
-train_size = int(0.8 * len(x_data))
-val_size = int(0.1 * len(x_data))
-test_size = len(x_data) - train_size - val_size
+# Splitta il dataset in train, validation, e test sets
+x_train = x_data[:num_train_samples]
+y_train = y_data[:num_train_samples]
+x_val = x_data[num_train_samples:num_train_samples+num_val_samples]
+y_val = y_data[num_train_samples:num_train_samples+num_val_samples]
+x_test = x_data[num_train_samples+num_val_samples:]
+y_test = y_data[num_train_samples+num_val_samples:]
 
-# 80% dei dati è per il training, 10% per il validation set, 10% per il test set
-training_dataset = training_dataset.shuffle(len(training_dataset)).batch(8).take(train_size)
-val_dataset = training_dataset.skip(train_size).take(val_size)
-test_dataset = training_dataset.skip(train_size + val_size).take(test_size)
-
-model = keras.Sequential()
+model = tf.keras.Sequential()
 model.add(Convolution2DFlipout(32, kernel_size=3, activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Convolution2DFlipout(64, kernel_size=3, activation='relu'))
@@ -59,7 +72,12 @@ model.compile(optimizer=opt,
               loss="categorical_crossentropy",
               metrics="accuracy")
 
-history = model.fit(training_dataset, epochs=20, validation_data=val_dataset)
+# model.build(x_data.shape)
+# model.summary()
+
+history = model.fit(x_train,y_train, epochs=20, validation_data=(x_val,y_val))
 
 model.save('LR0005.h5py')
-# model.evaluate(test_dataset)
+
+# model.evaluate()
+# model.predict(x_test)
