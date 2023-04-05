874d56586f738a81c01b597f312b8f12a172e8e2
Emanuele Peratello <116254592+EmaThatBoi@users.noreply.github.com>
2023-04-05 13:03:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import numpy as np
 import tensorflow as tf
 import functions
 import os
+from tensorflow import keras
+from keras.layers import MaxPooling2D,Dropout,Flatten
+from keras.models import Sequential
 from tensorflow_probability.python.layers import Convolution2DFlipout, DenseFlipout
-from tensorflow.python.keras.layers import MaxPooling2D, Flatten, Dropout
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.preprocessing import LabelEncoder
 
 # script pre-processing.py -> spettri suddivisi per diagnosi nelle corrispondenti directory
 #                             ndarray paddati con 0.
@@ -23,61 +27,68 @@ for i in range(len(data)):
     data[i] = ((arr - mean / std), label)
 
 # crea la coppia - (spettrogramma, label corrispondente)
+
 x_data, y_data = functions.prepare_data(data, dir_label)
 
 x_data = np.array(x_data, dtype=np.float32)
 y_data = np.array(y_data, dtype=np.float32)
 
-# Get the number of samples
+#undersampling
+x_data = x_data.reshape(x_data.shape[0],-1)  # shape diventa (920, 128*1723)
+rus = RandomUnderSampler(sampling_strategy=1, random_state=0)
+x_data,y_data = rus.fit_resample(x_data,y_data)
+x_data = x_data.reshape(x_data.shape[0],128,1723)
+
+# numero dei campioni
 num_samples = x_data.shape[0]
 
 # Shuffle dei dati
 indices = np.random.permutation(num_samples)
+
 x_data = x_data[indices]
 y_data = y_data[indices]
 
-# numero di campioni per ogni set
 train_ratio = 0.7
 val_ratio = 0.15
 test_ratio = 0.15
+
+# numero dei campioni per ogni set 
 num_train_samples = int(train_ratio * num_samples)
 num_val_samples = int(val_ratio * num_samples)
 num_test_samples = int(test_ratio * num_samples)
 
-x_data = tf.expand_dims(x_data,axis=3,name='channels')
-
-# Splitta il dataset in train, validation, e test sets
+x_data = np.expand_dims(x_data,axis=3)
+# Split il dataset in train, validation, and test set
 x_train = x_data[:num_train_samples]
 y_train = y_data[:num_train_samples]
+#validation set
 x_val = x_data[num_train_samples:num_train_samples+num_val_samples]
 y_val = y_data[num_train_samples:num_train_samples+num_val_samples]
+# test set
 x_test = x_data[num_train_samples+num_val_samples:]
 y_test = y_data[num_train_samples+num_val_samples:]
 
-model = tf.keras.Sequential()
-model.add(Convolution2DFlipout(32, kernel_size=3, activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Convolution2DFlipout(64, kernel_size=3, activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Convolution2DFlipout(128, kernel_size=3, activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Flatten())
-model.add(DenseFlipout(256, activation='relu'))
-model.add(Dropout(0.5))
-model.add(DenseFlipout(8, activation='softmax'))
+model = Sequential([
+    Convolution2DFlipout(32, kernel_size=3, activation='relu',activity_regularizer=None),
+    MaxPooling2D(pool_size=(2,2)),
+    Convolution2DFlipout(64, kernel_size=3, activation='relu',activity_regularizer=None),
+    MaxPooling2D(pool_size=(2,2)),
+    Convolution2DFlipout(128, kernel_size=3, activation='relu',activity_regularizer=None),
+    MaxPooling2D(pool_size=(2,2)),
 
-opt = tf.keras.optimizers.Adam(learning_rate=0.0005)
+    Flatten(),
+    DenseFlipout(256, activation='relu'),
+    Dropout(0.6),
+    DenseFlipout(8,activation='softmax')
+])
+
+opt = tf.keras.optimizers.Adam(learning_rate=1e-3)
 model.compile(optimizer=opt,
-              loss="categorical_crossentropy",
-              metrics="accuracy")
+              loss='categorical_crossentropy',
+              metrics=["accuracy"])
 
-# model.build(x_data.shape)
-# model.summary()
+history = model.fit(x_train,y_train, epochs=100, validation_data=(x_val,y_val),batch_size=(8))
 
-history = model.fit(x_train,y_train, epochs=20, validation_data=(x_val,y_val))
-
-model.save('LR0005.h5py')
-
-# model.evaluate()
-# model.predict(x_test)
+#model.save('LR002.h5py')
+#model.evaluate(test_dataset)
