d4890ad87f94e1c40ebf708a1aa79b4c68f120bc
Emanuele Peratello <116254592+EmaThatBoi@users.noreply.github.com>
2023-05-13 16:18:46 +0200

    Update pre-processing.py

--- pre-processing.py
+++ pre-processing.py
@@ -1,55 +1,50 @@
 import numpy as np
 import pandas as pd
 import os
-import librosa
-from librosa import display
 import matplotlib.pyplot as plt
+from librosa import load,feature,power_to_db
 
-def spectrogram(path):
-    samples, sr = librosa.load(path, sr=None)
-    signal = librosa.feature.melspectrogram(y=samples,
-                                            sr=sr)
-    signal_dB = librosa.power_to_db(signal)
+patient_diagnosis = pd.read_csv('archive/patient_diagnosis.csv', names=('ID', 'Diagnosis'))
+patient_diagnosis = np.array(patient_diagnosis)
 
-    #fig = plt.subplot()
-    #spectrogram = librosa.display.specshow(signal_dB)
+diagnosis = []
+for el in patient_diagnosis[:, 1]:
+    if el in diagnosis:
+        pass
+    else:
+        diagnosis.append(el)
 
-    return signal_dB
+diagnosis = np.array(diagnosis)
 
+results = []
+for file_name in os.listdir('archive/audio_and_txt_files/'):
 
-if __name__ == '__main__':
-    patient_diagnosis = pd.read_csv('archive/patient_diagnosis.csv', names=('ID', 'Diagnosis'))
-    patient_diagnosis = np.array(patient_diagnosis)
+    if file_name.endswith('.txt'):
+        breathing = pd.read_csv(('archive/audio_and_txt_files/'+file_name),sep='\t', names=('start','end','s','c'))
+        start = [breathing['start'][i] for i in range(len(breathing))]
+        end = [breathing['end'][i] for i in range(len(breathing))]
 
-    diagnosis = []
-    for el in patient_diagnosis[:, 1]:
-        if el in diagnosis:
-            pass
-        else:
-            diagnosis.append(el)
+        for nome_diagnosi in patient_diagnosis:
+            if str(file_name)[0:3] == str(nome_diagnosi[0]):                      # se è lo stesso paziente 101 == 101
+                directory = str(nome_diagnosi[1])                               # assegna nome directory
+                if not os.path.exists(os.path.join('diagnosi/', directory)):    # crea directory se non esiste
+                    os.mkdir('diagnosi/'+directory)                             # crea solo directory poichè mkdir crea il path più a destra
 
-    diagnosis = np.array(diagnosis)
-
-    results = []
-    for file_name in os.listdir('archive/audio_and_txt_files/'):
-        if file_name.endswith('.wav'):
-            for nome_diagnosi in patient_diagnosis:
-                if str(file_name)[0:3] == str(nome_diagnosi[0]):                      # se è lo stesso paziente 101 == 101
-                    directory = str(nome_diagnosi[1])                               # assegna nome directory
-                    if not os.path.exists(os.path.join('diagnosi/', directory)):    # crea directory se non esiste
-                        os.mkdir('diagnosi/'+directory)                             # crea solo directory poichè mkdir crea il path più a destra
-
-                    result = spectrogram(os.path.join('archive/audio_and_txt_files/', file_name))
-                    if result.shape[1] < 1723:                                      # se array è più corto degli altri paddalo
-                        result = np.pad(result, ( (0,0), (0, 1723 - result.shape[1]) ),mode='constant')
-
-                    np.save(os.path.join('diagnosi/', directory, file_name[:-4] + '.npy'), result)
-                    results.append(result)
+                path = os.path.join('archive/audio_and_txt_files/', str(file_name[0:-4] + '.wav'))
+                samples, sr = load(path, sr=44100)
 
 
+                for i in range(len(start)):
+                    start_sample = int(start[i] * sr)
+                    end_sample = int(end[i] * sr)
+                    breath_cycle_samples = samples[start_sample:end_sample]
 
+                    signal = feature.melspectrogram(y=breath_cycle_samples, sr=44100, hop_length=256)
+                    signal_dB = power_to_db(signal)
 
+                    if signal_dB.shape[1] < 500:                                      # se array è più corto degli altri paddalo
+                        signal_dB = np.pad(signal_dB, ( (0,0), (0, 500 - signal_dB.shape[1]) ),mode='constant')
+                    np.matrix.resize(signal_dB,(128,500))
 
-
-
-
+                    np.save(os.path.join('diagnosi/', directory, file_name[:-4] +'part_'+str(i) +'.npy'), signal_dB)
+                    results.append(signal_dB)
